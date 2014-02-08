c167e8b28cc7c69546b52de54f383bff2a40c4b4
Roger Hutchings <roger@incuna.com>
2014-02-08 01:12:07 +0000

    Typo in js, use padding for dl

--- _js/hover.js
+++ _js/hover.js
@@ -2,15 +2,17 @@ jQuery(document).ready(function ($) {
 
     'use strict';
 
+    var hoverClass = 'js-hover';
+
     // Apply a hover class to dl items with a matching data-slug attribute
     var definitions = $('dt, dd');
 
         definitions.mouseenter(function () {
-            $(this).parent().children('[data-slug="' + $(this).data('slug') + '"]').addClass('js-hover');
+            $(this).parent().children('[data-slug="' + $(this).data('slug') + '"]').addClass(hoverClass);
         })
         .mouseleave(function () {
             // Remove from all to prevent any weird edge cases
-            definitions.removeClass('hover');
+            definitions.removeClass(hoverClass);
         });
 
 
