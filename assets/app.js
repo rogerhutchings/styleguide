8ad149ee4b3754369992ee99637ea50f44638c0d
Roger Hutchings <roger@incuna.com>
2014-02-08 00:18:52 +0000

    More robust hover js

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,17 @@
 
 jQuery(document).ready(function ($) {
 
-    // Apply a hover class to dl items with a matching data-slug attribute
-    $('dt, dd').hover(function() {
-        $(this).parent().children('[data-slug="' + $(this).data('slug') + '"]').toggleClass('hover');
-    });
+    // // Apply a hover class to dl items with a matching data-slug attribute
+    var definitions = $('dt, dd');
+
+        definitions.mouseenter(function () {
+            $(this).parent().children('[data-slug="' + $(this).data('slug') + '"]').addClass('hover');
+        })
+        .mouseleave(function () {
+            // Remove from all to prevent any weird edge cases
+            definitions.removeClass('hover');
+        });
+
 
 });
 
