f00156102761e5bbed1c2548858c1e305055729e
Roger Hutchings <roger@incuna.com>
2014-02-08 00:03:54 +0000

    Tidying up

--- _js/hover.js
+++ _js/hover.js
@@ -2,9 +2,7 @@ jQuery(document).ready(function ($) {
 
     // Apply a hover class to dl items with a matching data-slug attribute
     $('dt, dd').hover(function() {
-        var slug = $(this).data('slug');
-        $(this).parent().children('[data-slug="' + slug + '"]').toggleClass('hover');
+        $(this).parent().children('[data-slug="' + $(this).data('slug') + '"]').toggleClass('hover');
     });
 
 });
-
