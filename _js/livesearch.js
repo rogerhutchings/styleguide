fc9146b07222ad8c8f71b0fef3b8d74d87213a75
Roger Hutchings <roger@incuna.com>
2014-02-08 22:08:00 +0000

    Toggle nav elements on search

--- _js/livesearch.js
+++ _js/livesearch.js
@@ -3,10 +3,17 @@ jQuery(document).ready(function ($) {
     'use strict';
 
     var hideClass = 'js-hide';
+    var disabledClass = 'js-disabled';
 
     // Save selectors
     var searchElements = $('.definitions').find('dt, dd');
     var sectionTitles = $('h2');
+    var sidebarNav = $('.sidebar-alphabet').find('a');
+
+    var sections = [];
+    for (var i = 97; i <= 122; i++) {
+        sections[sections.length] = 'section-' + String.fromCharCode(i);
+    }
 
     // Add in a delay so it doesn't go mental on every keystroke
     // http://stackoverflow.com/questions/1909441/jquery-keyup-delay
@@ -25,6 +32,7 @@ jQuery(document).ready(function ($) {
         if (searchTerm === '') {
             searchElements.removeClass(hideClass);
             sectionTitles.removeClass(hideClass);
+            updateNav();
             return;
         }
 
@@ -44,9 +52,34 @@ jQuery(document).ready(function ($) {
         $.each(slugs, function(index, slug) {
             var matches = searchElements.filter("[data-slug=" + slug + "]");
             matches.removeClass(hideClass);
+            // Remove hide class from section title
             matches.parent().prev().removeClass(hideClass);
         });
 
+        // Update the nav
+        updateNav();
+
+    };
+
+    var updateNav = function() {
+
+        // Reset behaviour
+        sidebarNav.removeClass(disabledClass);
+        sidebarNav.off('click').on('click', function (e) {
+            return true;
+        });
+
+        // Add disabled class
+        var disabledSections = sectionTitles.filter('.' + hideClass).map(function () {
+            return '[href="#' + $(this).children('a').attr('name') + '"]';
+        }).get().join();
+        sidebarNav.filter(disabledSections).addClass(disabledClass);
+
+        // Disable links
+        sidebarNav.filter('.' + disabledClass).off('click').on('click', function (e) {
+            e.preventDefault();
+            return false;
+        });
     };
 
     // Using proxy to define context
